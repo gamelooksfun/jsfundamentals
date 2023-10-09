beafd07cf8884246c624dc559470d4d8d5e515d9
looks <108483411+gamelooksfun@users.noreply.github.com>
2023-10-09 16:02:50 -0700

    getComputerChoice() with switch conditional

--- rps1/script.js
+++ rps1/script.js
@@ -1,13 +1,25 @@
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        return choices[0];
-    } else if (randomNumber === 1) {
-        return choices[1];
-    } else {
-        return choices[2];
+    switch (randomNumber) {
+        case 0:
+            return choices[0];
+            break;
+        case 1:
+            return choices[1];
+            break;
+        case 2:
+            return choices[2];
+            break;
     }
+    
+    // if (randomNumber === 0) {
+    //     return choices[0];
+    // } else if (randomNumber === 1) {
+    //     return choices[1];
+    // } else {
+    //     return choices[2];
+    // }
 }
 
 console.log(getComputerChoice());
