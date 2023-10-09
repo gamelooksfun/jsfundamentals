26ca8e08858df0f888a98634c06a5b9fcd319d1c
looks <108483411+gamelooksfun@users.noreply.github.com>
2023-10-09 16:21:57 -0700

    playRound() function working in browser console

--- rps1/script.js
+++ rps1/script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
             return choices[2];
             break;
     }
-    
+
     // if (randomNumber === 0) {
     //     return choices[0];
     // } else if (randomNumber === 1) {
@@ -22,4 +22,28 @@ function getComputerChoice() {
     // }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You lose! Paper beats Rock';
+        } else {
+            return 'You win! Rock beats Scissors';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You win! Paper beats Rock';
+        } else {
+            return 'You lose! Scissors beats Paper';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'You lose! Rock beats Scissors';
+        } else {
+            return 'You win! Scissors beats Paper';
+        }
+    }
+}
+
+console.log(playRound('rock', getComputerChoice()));
